29b5bc03b85b3e2c1857797fbd6e81d604d7e841
Joe Caple <32098792+Caple-JC-BPC-Magenta@users.noreply.github.com>
2017-10-31 13:43:25 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+ <html> 
+ <head> 
+ <title>test 01</title> 
+ </head> 
+ <body> 
+ <?php 
+ $fName = "Joe"; 
+ $sName = "Caple"; 
+ $fullname = $fName . " " . $sName; 
+  
+ echo $fullname; 
+  
+ if ($fullname == "Joe Caple") { 
+ echo "By god what a guy"; 
+ } 
+ elseif 
+ ($fullname == "Patrick Douel") { 
+ echo "Yo me is computer nard i wan to liek men"; 
+ } 
+ else 
+ { 
+ echo "Who are ya boyo?"; 
+ }  
+  
+ ?> 
+ </body> 
+ </html> 
